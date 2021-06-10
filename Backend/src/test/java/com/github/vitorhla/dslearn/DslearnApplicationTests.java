754dd2aefe11e8f106f75ab205890ad2769637ab
vitorhla <vitorhenriquealt@gmail.com>
2021-06-09 21:00:51 -0300

    Backend Project created

---
+++ Backend/src/test/java/com/github/vitorhla/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vitorhla.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
