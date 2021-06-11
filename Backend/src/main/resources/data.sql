96a7924b629bf100628b9d41630f6502caa932f1
vitorhla <vitorhenriquealt@gmail.com>
2021-06-11 18:39:44 -0300

    Offer

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -14,3 +14,11 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name,img_Uri, img_Gray_Uri) VALUES ('Curso HTML', 'https://prod-discovery.edx-cdn.org/media/course/image/0e575a39-da1e-4e33-bb3b-e96cc6ffc58e-8372a9a276c1.png', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_Id) VALUES (1.0, TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_Id) VALUES (2.0, TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
+
+
+
+
+
