ebcd5bacbb90f1ab392b1946c143e209cc96ce84
vitorhla <vitorhenriquealt@gmail.com>
2021-06-12 08:32:31 -0300

    Resource

--- Backend/src/main/java/com/github/vitorhla/dslearn/entities/Offer.java
+++ Backend/src/main/java/com/github/vitorhla/dslearn/entities/Offer.java
@@ -2,14 +2,16 @@ package com.github.vitorhla.dslearn.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -27,6 +29,10 @@ public class Offer implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
+	
+	@OneToMany(mappedBy =  "offer")
+	private List<Resource> resources = new ArrayList<>();
+	
 	
 	public Offer () {
 		
@@ -70,6 +76,14 @@ public class Offer implements Serializable {
 	public void setCourse(Course course) {
 		this.course = course;
 	}
+	
+	public List<Resource> getResources() {
+		return resources;
+	}
+	
+	
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -18,6 +18,10 @@ INSERT INTO tb_course (name,img_Uri, img_Gray_Uri) VALUES ('Curso HTML', 'https:
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_Id) VALUES (1.0, TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_Id) VALUES (2.0, TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
 
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML','Trilha principal do curso', 1,'https://prod-discovery.edx-cdn.org/media/course/image/0e575a39-da1e-4e33-bb3b-e96cc6ffc58e-8372a9a276c1.png', 1,1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum','Tire suas duvidas', 2,'https://prod-discovery.edx-cdn.org/media/course/image/0e575a39-da1e-4e33-bb3b-e96cc6ffc58e-8372a9a276c1.png', 2,1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives ','Lives exclusivas para a turma ', 3,'https://prod-discovery.edx-cdn.org/media/course/image/0e575a39-da1e-4e33-bb3b-e96cc6ffc58e-8372a9a276c1.png', 0,1);
+
 
 
 
